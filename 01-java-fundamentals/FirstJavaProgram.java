02e3475f684cbeff2ddc858ef0708b98da3d3b86
triciadrich <36164322+triciadrich@users.noreply.github.com>
2021-08-28 14:24:18 -0700

    first java

---
+++ 01-java-fundamentals/FirstJavaProgram.java
@@ -0,0 +1,7 @@
+public class FirstJavaProgram {
+    public static void main(String[] args) {
+        System.out.println("Hello my name is Patricia ");
+        System.out.println("I am 40 years old");
+        System.out.println("My hometown is Whiteford, Maryland");
+    }
+}
