6c4cf2b2547d1ea8de492ab5246d6adb8f0ad261
triciadrich <36164322+triciadrich@users.noreply.github.com>
2021-09-10 22:53:16 -0700

    part 2

---
+++ 02-java-oop/03-zoo-keeper-pt1/Bat.java
@@ -0,0 +1,28 @@
+public class Bat extends Mammal{
+
+    public Bat(int energyLevel) {
+        super(energyLevel);
+        
+    }
+
+    public void fly(){
+        if (energyLevel == 0){
+            System.out.println("Not enough energy, eat a human");
+            return;
+        }else{
+            energyLevel = energyLevel -= 50;
+            System.out.println("Bat is soaring through the sky, your energy is now " + getEnergy() );
+        }
+    }
+
+    public void eatHuman(){
+        energyLevel = energyLevel += 25;
+        System.out.println("Yum, your energy is now: " + getEnergy());
+    }
+
+    public void attackTown(){
+        energyLevel = energyLevel -= 100;
+        System.out.println("The town is on fire!!! Energy left is: " + getEnergy());
+    }
+    
+}
---
+++ 02-java-oop/03-zoo-keeper-pt1/BatTest.java
@@ -0,0 +1,13 @@
+public class BatTest {
+    public static void main(String[] args) {
+        Bat bat = new Bat(300);
+        bat.displayEnergy();
+        bat.attackTown();
+        bat.attackTown();
+        bat.attackTown();
+        bat.eatHuman();
+        bat.eatHuman();
+        bat.fly();
+        bat.fly();
+    }
+}
